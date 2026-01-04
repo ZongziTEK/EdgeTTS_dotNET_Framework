dc0fce73c9bd15d794122b208b1725e37618cfd8
ZongziTEK <stbbrd@outlook.com>
2026-01-05 00:54:07 +0800

    0

--- edge_tts_test/Program.cs
+++ edge_tts_test/Program.cs
@@ -1,14 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Edge_tts_sharp;
 using Edge_tts_sharp.Model;
-using Edge_tts_sharp.Utils;
-using System.Threading.Tasks;
-using System.Web;
 
 Edge_tts.Await = true;
 PlayOption option = new PlayOption
 {
-    Rate = 1,
+    Rate = 0,
     Text = ""
 };
 string msg = string.Empty;
